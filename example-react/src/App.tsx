f058bac6e310f46d9ed78ec2a1ecb8b068756b47
Felix Roos <flix91@gmail.com>
2023-08-22 13:55:02 +0200

    use useSdk

--- example-react/src/App.tsx
+++ example-react/src/App.tsx
@@ -1,28 +1,40 @@
-// @ts-ignore
-import { sdk, act, query } from "../../src/index.mjs";
 import { useState } from "react";
-import useSWR from "swr";
+import useSdk from "./useSdk";
 
-function useSdk(config: any) {
-  return useSWR([config ? query(config) : null], () => act(config));
-}
+const config = {
+  env: "stage",
+  dmShortID: "83cc6374",
+};
 
 function App() {
   const [id, setID] = useState("IVH5LQglkL");
+  const [model, setModel] = useState("muffin");
 
   const { data: entry } = useSdk({
+    ...config,
     action: "getEntry",
-    env: "stage",
-    dmShortID: "83cc6374",
     entryID: id,
     model: "muffin",
+  });
+
+  const { data: entryList } = useSdk({
+    ...config,
+    action: "entryList",
+    model,
   });
 
   return (
     <>
       <h1>entry: {entry?._entryTitle}</h1>
-      <button onClick={() => setID("frcjP1xMmi3")}>Kiwi King</button>
-      <button onClick={() => setID("IVH5LQglkL")}>hello fdk</button>
+      <button onClick={() => setID("frcjP1xMmi3")}>muffin: Kiwi King</button>
+      <button onClick={() => setID("IVH5LQglkL")}>muffin: hello fdk</button>
+      <pre>{JSON.stringify(entry || {})}</pre>
+      <h1>List {model}</h1>
+      <button onClick={() => setModel("muffin")}>model: muffin</button>
+      <button onClick={() => setModel("baker")}>model: baker</button>
+      {entryList?.items.map((item: any) => (
+        <li key={item.id}>{item._entryTitle}</li>
+      ))}
     </>
   );
 }
