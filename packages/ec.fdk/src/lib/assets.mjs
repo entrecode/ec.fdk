37845b590350eb53404a4ccb3cf2d2b1d10c0179
Michael Locher <michael_locher@web.de>
2024-01-24 09:13:22 +0100

    added list breaker for assetList

--- packages/ec.fdk/src/lib/assets.mjs
+++ packages/ec.fdk/src/lib/assets.mjs
@@ -1,11 +1,11 @@
-import { expect, query, apiURL, fetcher } from "./util.mjs";
+import { apiURL, expect, fetcher, query } from './util.mjs';
 
 export async function getAsset({ env, dmShortID, assetGroup, assetID, token }) {
   expect({ env, dmShortID, assetGroup, assetID });
   const q = query({ assetID: assetID });
   const url = apiURL(`a/${dmShortID}/${assetGroup}?${q}`, env);
   const list = await fetcher(url, { dmShortID, token });
-  return list._embedded["ec:dm-asset"];
+  return list._embedded['ec:dm-asset'];
 }
 
 export async function assetList(config) {
@@ -16,24 +16,17 @@ export async function assetList(config) {
   const q = query(options);
   const url = apiURL(`a/${dmShortID}/${assetGroup}?${q}`, env);
   const { count, total, _embedded } = await fetcher(url, { dmShortID, token });
-  const items = _embedded ? _embedded[`ec:dm-asset`] : [];
+  let items = _embedded ? _embedded['ec:dm-asset'] : [];
+  items = !Array.isArray(items) ? [items] : items;
   return { count, total, items };
 }
 
-export async function createAsset({
-  env,
-  dmShortID,
-  assetGroup,
-  token,
-  file,
-  name,
-  options,
-}) {
+export async function createAsset({ env, dmShortID, assetGroup, token, file, name, options }) {
   expect({ env, dmShortID, assetGroup, file });
 
   const url = apiURL(`a/${dmShortID}/${assetGroup}`, env);
   const formData = new FormData();
-  formData.append("file", file, name);
+  formData.append('file', file, name);
   if (options) {
     Object.keys(options).forEach((key) => {
       formData.append(key, options[key]);
@@ -44,9 +37,9 @@ export async function createAsset({
     url,
     { token },
     {
-      method: "POST",
+      method: 'POST',
       body: formData,
     },
   );
-  return list._embedded["ec:dm-asset"];
+  return list._embedded['ec:dm-asset'];
 }
