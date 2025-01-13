5de7999da65ac9e4c613da670f9b694322cfe9cf
Felix Roos <flix91@gmail.com>
2025-01-13 11:01:02 +0100

    fix: getAsset "Cannot read properties of undefined"

--- packages/ec.fdk/src/lib/assets.ts
+++ packages/ec.fdk/src/lib/assets.ts
@@ -4,8 +4,8 @@ export async function getAsset({ env, dmShortID, assetGroup, assetID, token }) {
   expect({ env, dmShortID, assetGroup, assetID });
   const q = query({ assetID: assetID });
   const url = apiURL(`a/${dmShortID}/${assetGroup}?${q}`, env);
-  const list = await fetcher(url, { token });
-  return list._embedded["ec:dm-asset"];
+  const { _embedded } = await fetcher(url, { token });
+  return _embedded ? _embedded["ec:dm-asset"] : undefined;
 }
 
 export async function assetList(config) {
